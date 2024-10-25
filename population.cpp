7a7fa0b8ce5e99e115137d77929ff54002b8609d
RhysticPatrol <stevenswilson7798@gmail.com>
2024-10-25 12:36:16 -0400

    Update population.cpp

--- population.cpp
+++ population.cpp
@@ -4,31 +4,31 @@ using namespace std;
 
 int main()
 {
-    int organisms;
+    int organisms; //setting up variables
     int populate;
     int days;
     cout << "Please enter the starting population size:\n";
     cin >> organisms;
-    if(organisms < 2)
+    if(organisms < 2) // restriction
     {
         cout << "The population can not reproduce if the population is less than 2";
         return 0;
     }
     cout << "Now pleae enter the daily population increase:\n";
     cin >> populate;
-    if(populate < 0)
+    if(populate < 0) // restriction
     {
         cout << "This program is designed to calculate popualation increase not decreasing.";
         return 0;
     }
     cout << "Finally, enter the number of days to calculate:\n";
     cin >> days;
-    if(days < 1)
+    if(days < 1) // restriction
     {
         cout << "Number of days less than one are not valid in this programm. Please try again.";
         return 0;
     }
-    for(int final; final <= days; ++final)
+    for(int final; final <= days; ++final) //loop to print results
     {
         int populate = populate * organisms;
         cout << populate << endl;
